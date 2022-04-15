092cdeca2ca188a95e8f91bc548b02a5c950aab8
neo-dude <neo-dude@hushmail.xyz>
2022-04-15 15:38:08 -0400

    15 April Need to: figure out why the data is clustered so much.

--- Final/functions.py
+++ Final/functions.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 import pandas as pd
+import plotly.graph_objects as go
+
 
 def import_data(ticker, start_date, end_date, interval):
     """
@@ -29,3 +31,33 @@ def apply_contango(df1, df2):
         temp.iloc[i] = check_contango(df1.iloc[i]['Close'], df2.iloc[i]['Close'])
         
     return temp
+
+def my_plot_function(df, color='Virdis', title=''):
+    plot = go.Figure()
+    plot.add_trace(go.Mesh3d(
+            x = df['strike_price'], 
+            y = df['TTM'], 
+            z = df['impl_volatility'],
+            colorbar_title = 'IV',
+            colorscale = color, # Virdis
+            # This line is the key to the color gradient!!!!
+            #####
+            intensity = df['impl_volatility'],   ##### <- hard to find on the internet
+            #####
+            opacity = 0.7,
+            showscale = True))
+
+
+    plot.update_layout(title=title + ' Vol Surface',
+            # You have to pass this scene variable for labels, I have no idea why.
+            # I can't figure out how else to make the axis labels.
+            scene = dict(
+            xaxis_title = 'Strike',
+            yaxis_title = 'TTM',
+            zaxis_title = 'IV'),
+            # size
+            width=600,
+            height=600,
+            # reduce border size
+            margin=dict(l=20, r=20, b=40, t=40))
+    plot.show()
